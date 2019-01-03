21b9a41fbbe1404e066c044c50de7810cad9c5ac
Muhamad-Andriansyah <45659337+Muhamad-Andriansyah@users.noreply.github.com>
2019-01-03 08:44:13 +0700

    Add files via upload

---
+++ Program Memvalidasi Umur 1-100thn.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int umur;
+
+cout<<"PROGRAM MEMVALIDASI UMUR"<<endl;
+
+cout<<"Input Tahun Kelahiran Anda = ";
+cin>>umur;
+umur=2018-umur;
+cout<<"Umur Anda Adalah           = ";
+return 0;
+
+}
